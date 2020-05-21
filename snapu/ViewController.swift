3dd6b8c14bdd93a998d0db5eccfce6a231169705
Mayur Parmar <mrparmar2997@gmail.com>
2020-05-21 15:00:43 +0530

    Initial Commit

---
+++ snapu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snapu
+//
+//  Created by Mayur Parmar on 21/05/20.
+//  Copyright © 2020 Mayur Parmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
